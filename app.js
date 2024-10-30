01ff4d191cfef8e7c0a495a838716ebdeda43bd9
Praneeth1221 <praneethravishka@gmail.com>
2024-10-30 11:05:50 +0530

    Secound commit

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-console.log(Math.floor((Math.random()*10)+1));
+
+let rNum=Math.floor((Math.random()*10)+1);
+let count1=3;
+
+function log(){
+   if(count1>0){
+    let no= document.getElementById("number").value;
+    let dis= document.getElementById("dission");
+    let chance = document.getElementById("chance");
+    console.log(rNum);
+   
+    chance.innerHTML=("Chances to left : "+count1);
+    
+    if(no>rNum){
+        dis.innerHTML="Too High.Try Again";
+    }else if(no<rNum){
+        dis.innerHTML="Too :LOw..try Again";
+    }else{
+        dis.innerHTML="Correct..";
+        count1=0;
+    }
+    
+    
+    count1--;
+    
+    
+}
+}
+
+
+
 
